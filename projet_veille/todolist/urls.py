40e45a9fec5bbca3b0260bc902054e2f5c2676d8
Imem Belcadi <belcadiimem01@gmail.com>
2021-09-20 19:28:06 -0400

    ajout de la page details pour les taches

--- projet_veille/todolist/urls.py
+++ projet_veille/todolist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListeTache
+from .views import DetailTache, ListeTache
 
 urlpatterns = [
     path('', ListeTache.as_view(), name='tache'),
+    path('tache/<int:pk>/', DetailTache.as_view(), name='tache'),
 ]
--- projet_veille/todolist/views.py
+++ projet_veille/todolist/views.py
@@ -1,7 +1,12 @@
 from todolist.models import Tache
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Tache
 
 class ListeTache(ListView):
     model = Tache
+
+class DetailTache(DetailView):
+    model = Tache
+    context_object_name = "tache"
