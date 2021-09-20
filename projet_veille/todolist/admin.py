f3d341ed3e7aa3ee547974e468a848db76251f7e
Imem Belcadi <belcadiimem01@gmail.com>
2021-09-19 23:19:49 -0400

    base de donnés créer + page admin fonctionnel

--- projet_veille/todolist/admin.py
+++ projet_veille/todolist/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tache
+
+admin.site.register(Tache)
 
 # Register your models here.
--- projet_veille/todolist/models.py
+++ projet_veille/todolist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tache(models.Model):
+    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    titre = models.CharField(max_length=300)
+    description = models.TextField(null=True, blank=True)
+    termine = models.BooleanField(default=False)
+    cree = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titre
+
+    class Meta:
+        ordering = ['termine']
+
