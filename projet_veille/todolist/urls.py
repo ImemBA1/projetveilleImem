ab4098f0497ab677b79aad5dddc9169bc8cd127a
Imem Belcadi <belcadiimem01@gmail.com>
2021-09-21 00:04:43 -0400

    ajout tache fonctionnel

--- projet_veille/todolist/urls.py
+++ projet_veille/todolist/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DetailTache, ListeTache
+from .views import CreeTache, DetailTache, ListeTache
 
 urlpatterns = [
-    path('', ListeTache.as_view(), name='tache'),
+    path('', ListeTache.as_view(), name='taches'),
     path('tache/<int:pk>/', DetailTache.as_view(), name='tache'),
+    path('cree-tache/', CreeTache.as_view(), name='cree-tache'),
 ]
--- projet_veille/todolist/views.py
+++ projet_veille/todolist/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Tache
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 
 class ListeTache(ListView):
     model = Tache
@@ -10,3 +12,9 @@ class ListeTache(ListView):
 class DetailTache(DetailView):
     model = Tache
     context_object_name = "tache"
+
+class CreeTache(CreateView):
+    model = Tache
+    fields = '__all__' # tout les champs 
+    success_url = reverse_lazy('taches') #retour à la page d'accueil
+    
