f541bb6962eb436e2a6148de53eb7df31fc32d20
Imem Belcadi <belcadiimem01@gmail.com>
2021-09-21 13:58:51 -0400

    ajout du login, il ne retourne pas à la page d'acceuil

--- projet_veille/todolist/urls.py
+++ projet_veille/todolist/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import CreeTache, DetailTache, ListeTache, ModifierTache, SupprTache
+from .views import CreeTache, DetailTache, ListeTache, ModifierTache, SupprTache, LoginV
 
 urlpatterns = [
     path('', ListeTache.as_view(), name='taches'),
     path('tache/<int:pk>/', DetailTache.as_view(), name='tache'),
     path('cree-tache/', CreeTache.as_view(), name='cree-tache'),
     path('modif-tache/<int:pk>/', ModifierTache.as_view(), name='modif-tache'),
-    path('suppr-tache/<int:pk>/', SupprTache.as_view(), name='suppr-tache')
+    path('suppr-tache/<int:pk>/', SupprTache.as_view(), name='suppr-tache'),
+    path('login/', LoginV.as_view(), name='login')
+
 ]
--- projet_veille/todolist/views.py
+++ projet_veille/todolist/views.py
@@ -1,3 +1,4 @@
+import todolist
 from todolist.models import Tache
 from django.shortcuts import render
 from django.views.generic.list import ListView
@@ -5,6 +6,7 @@ from django.views.generic.detail import DetailView
 from .models import Tache
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
 
 class ListeTache(ListView):
@@ -30,4 +32,11 @@ class ModifierTache(UpdateView):
 
 class SupprTache(DeleteView):
     model = Tache
+    success_url = reverse_lazy('taches') 
+
+
+class LoginV(LoginView):
+    template_name = "todolist/login.html"
+    fields = '__all__'
+    redirect_authenticated_user = True
     success_url = reverse_lazy('taches') 
