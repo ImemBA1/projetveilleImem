f06fe41534e7c716b4e48008116f25804625d2f9
Imem Belcadi <belcadiimem01@gmail.com>
2021-09-21 11:43:58 -0400

    supprimer une tache fonctionnel

--- projet_veille/todolist/views.py
+++ projet_veille/todolist/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Tache
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
@@ -24,6 +24,10 @@ class CreeTache(CreateView):
 
 class ModifierTache(UpdateView):
     model = Tache
-    fields = '__all__' # tout les champs 
-    success_url = reverse_lazy('taches') #retour à la page d'accueil
-    
+    fields = '__all__'
+    success_url = reverse_lazy('taches') 
+
+
+class SupprTache(DeleteView):
+    model = Tache
+    success_url = reverse_lazy('taches') 
