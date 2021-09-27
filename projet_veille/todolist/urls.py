bdd259bcb19529735b89989c96571030a2c929ed
Imem Belcadi <belcadiimem01@gmail.com>
2021-09-26 22:00:54 -0400

    logout fonctionnel

--- projet_veille/todolist/urls.py
+++ projet_veille/todolist/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', ListeTache.as_view(), name='taches'),
@@ -7,6 +8,7 @@ urlpatterns = [
     path('cree-tache/', CreeTache.as_view(), name='cree-tache'),
     path('modif-tache/<int:pk>/', ModifierTache.as_view(), name='modif-tache'),
     path('suppr-tache/<int:pk>/', SupprTache.as_view(), name='suppr-tache'),
-    path('login/', LoginV.as_view(), name='login')
+    path('login/', LoginV.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout')
 
 ]
